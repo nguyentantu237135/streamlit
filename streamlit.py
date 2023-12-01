51bcc7f77afcd45d0de143d8ec81bbcf61de23e9
nguyentantu237135 <nguyentantu230704@gmail.com>
2023-12-01 23:03:52 +0700

    cập nhật

--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import time
 
-with st.spinner("wait for it..."):
+with st.spinner("chờ xíu..."):
     time.sleep(3)
-st.success('Done') 
+st.success('Xong')
+ten = st.text_input("Nhập vào tên của bạn: ") 
+click = st.button("Nhấn vào đây")
+if click:
+    st.write("Tên của bạn là: ", ten, "chó điên")
 
