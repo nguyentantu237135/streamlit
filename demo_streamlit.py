e7b959c0666029103ec911371db0da00f9c1fcc5
nguyentantu237135 <nguyentantu230704@gmail.com>
2023-12-01 23:11:19 +0700

    cập nhật

--- demo_streamlit.py
+++ demo_streamlit.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import time
 
-with st.spinner("chờ xíu..."):
+with st.spinner("Chờ xíu..."):
     time.sleep(3)
 st.success('Xong')
 ten = st.text_input("Nhập vào tên của bạn: ") 
